1acc4e90b077dacd6bea21507aa9281e0529f39b
Galorn <alexis.teston@ecam-strasbourg.eu>
2016-02-29 17:26:30 +0100

    Main code
    Working code for 1 subject, undefined number of marks.
    Give the average

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    // INITIALISATION DES VARIABLES
+    string subject("");
+    vector<int> mark;
+    int markNumber(0) , note(0), i(0), taille(0);
+    double average(0);
+
+
+    // ACCUEIL
+    cout << "Hello Student!" << endl;
+    cout << "Please enter the subject of your notes" << endl;
+    cin >> subject ;
+    cout << "You chose " << subject << " Good" << endl;
+    cout << endl << "Please enter the number of marks" << endl;
+    cin >> markNumber ;
+
+    // REMPLISSAGE DU TABLEAU DE NOTE
+    if (markNumber==0)
+    {
+        cout << "Wrong key." << endl;
+        cout << "Okay ! " << endl;
+
+    }
+    else
+    {
+        cout << "You chose to enter " << markNumber << " marks. " << endl;
+        cout << "Enter your mark one by one please" << endl ;
+
+        for (i=0; i < markNumber ; i++)
+        {
+            cin >> note ;
+            mark.push_back(note);
+
+        }
+        taille = mark.size();
+        cout << "You entered " << taille << " marks." << endl;
+        for(i=0; i<mark.size(); ++i)
+        {
+            cout << "You entered " << mark[i] << "." << endl;
+            average += mark[i];
+        }
+    average /= mark.size();
+
+
+    cout << "Your average mark on this subject is " << average << endl;
+
+    }
+
+
+    return 0;
+}
